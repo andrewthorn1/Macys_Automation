6890abb49a2edb4557b89e78cf5044c1a883317d
LAPTOP-K7Q969NR\andre <andrewthorn1@gmail.com>
2022-07-23 18:24:18 -0500

    Add test_search.py. Moved 2 tests in.

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -14,22 +14,6 @@ class TestHomepage:
         expected_links = homepage_nav.NAV_LINK_TEXT
         assert expected_links == actual_links, 'Validating Header Nav Links Text'
 
-    def test_search(self):
-        homepage_nav = HomepageNav(self.driver)
-        homepage_nav.get_search().send_keys('Men shoes')
-        homepage_nav.get_search().send_keys(Keys.RETURN)
-        search_nav = SearchNav(self.driver)
-        search_first_item = search_nav.get_first_item()
-        assert search_first_item is not None
-        print(search_first_item.text)
-
-    def test_search_not_found(self):
-        homepage_nav = HomepageNav(self.driver)
-        homepage_nav.get_search().send_keys(']]')
-        homepage_nav.get_search().send_keys(Keys.RETURN)
-        search_nav = SearchNav(self.driver)
-        search_not_found = search_nav.get_not_found_message()
-        assert 'We couldn’t find a match for your search.' in search_not_found.text
 
 
 
---
+++ tests/test_search.py
@@ -0,0 +1,34 @@
+import time
+
+import pytest
+from pom.search_nav import SearchNav
+from pom.homepage_nav import HomepageNav
+from selenium.webdriver import Keys
+
+@pytest.mark.usefixtures('setup')
+class TestSearch:
+
+    def test_search(self):
+        homepage_nav = HomepageNav(self.driver)
+        homepage_nav.get_search().send_keys('Men shoes')
+        homepage_nav.get_search().send_keys(Keys.RETURN)
+        search_nav = SearchNav(self.driver)
+        search_first_item = search_nav.get_first_item()
+        assert search_first_item is not None
+        # print(search_first_item.text)
+
+    def test_search_not_found(self):
+        homepage_nav = HomepageNav(self.driver)
+        homepage_nav.get_search().send_keys(']]')
+        homepage_nav.get_search().send_keys(Keys.RETURN)
+        search_nav = SearchNav(self.driver)
+        search_not_found = search_nav.get_not_found_message()
+        assert 'We couldn’t find a match for your search.' in search_not_found.text
+
+
+
+
+
+
+
+
