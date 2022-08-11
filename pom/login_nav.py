1b24154762fed78fd300ae466afbcc42042aa203
LAPTOP-K7Q969NR\andre <andrewthorn1@gmail.com>
2022-08-11 12:01:21 -0500

    Update 8/11/2022 Create test_create_account_bot_protect

--- pom/login_nav.py
+++ pom/login_nav.py
@@ -11,6 +11,15 @@ class LoginNav(SeleniumBase):
         self.__password_fild_locator: str = '#pw-input'
         self.__sign_in_button_locator: str = '#sign-in'
         self.__create_account_button_locator: str = '#standard-sign-up'
+        self.__ca_first_name_fild_locator: str = '#ca-profile-firstname'
+        self.__ca_last_name_fild_locator: str = '#ca-profile-lastname'
+        self.__ca_email_fild_locator: str = '#ca-profile-email'
+        self.__ca_password_fild_locator: str = '#ca-profile-password'
+        self.__ca_birthday_month_locator: str = '//select[@id="ca-profile-birth-month"]/option[text()="January"]'
+        self.__ca_birthday_day_locator: str = '//select[@id="ca-profile-birth-day"]/option[text()="01"]'
+        self.__ca_button_locator: str = '#ca-profile-submit'
+        self.__notification_message_locator: str = '//div[@class="show"]//*[@class="notification-body"]'
+
 
     def get_email_fild(self):
         return self.is_visible('css', self.__email_fild_locator)
@@ -27,3 +36,28 @@ class LoginNav(SeleniumBase):
     def refresh_page(self):
         self.driver.delete_all_cookies()
         self.driver.refresh()
+
+    def get_ca_first_name_fild(self):
+        return self.is_visible('css', self.__ca_first_name_fild_locator)
+
+    def get_ca_last_name_fild(self):
+        return self.is_visible('css', self.__ca_last_name_fild_locator)
+
+    def get_ca_email_fild(self):
+        return self.is_visible('css', self.__ca_email_fild_locator)
+
+    def get_ca_password_fild(self):
+        return self.is_visible('css', self.__ca_password_fild_locator)
+
+    def get_ca_birthday_month(self):
+        return self.is_present('xpath', self.__ca_birthday_month_locator)
+
+    def get_ca_birthday_day(self):
+        return self.is_present('xpath', self.__ca_birthday_day_locator)
+
+    def get_ca_button(self):
+        return self.is_visible('css', self.__ca_button_locator)
+
+    def get_ca_notification_message(self) -> WebElement:
+        return self.is_present('xpath', self.__notification_message_locator)
+
--- tests/integration/test_login.py
+++ tests/integration/test_login.py
@@ -19,11 +19,19 @@ class TestLogin:
         assert "Sorry, it looks like there's a problem on our end. " \
                "For assistance, please call" in search_nav.get_notification_message().text
 
-    def test_create_account(self):
+    def test_create_account_bot_protect(self):
         homepage_nav = HomepageNav(self.driver)
         login_nav = LoginNav(self.driver)
         search_nav = SearchNav(self.driver)
         homepage_nav.get_login().click()
         login_nav.get_create_account_button().click()
         login_nav.refresh_page()
-        time.sleep(3)
+        login_nav.get_ca_first_name_fild().send_keys("Test Name")
+        login_nav.get_ca_last_name_fild().send_keys("Test Surname")
+        login_nav.get_ca_email_fild().send_keys("Testemail@gmail.com")
+        login_nav.get_ca_password_fild().send_keys("Testpassword1")
+        login_nav.get_ca_birthday_month().click()
+        login_nav.get_ca_birthday_day().click()
+        login_nav.get_ca_button().click()
+        assert "We're sorry, but we cannot display your information" \
+               " at this time." in login_nav.get_ca_notification_message().text
